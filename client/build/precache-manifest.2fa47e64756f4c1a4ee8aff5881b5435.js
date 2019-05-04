ee95411d02c4179363276d981228132d3df1aadc
Ken Riley <kriley314@gmail.com>
2019-05-03 23:25:00 -0500

    New version of Books app

---
+++ client/build/precache-manifest.2fa47e64756f4c1a4ee8aff5881b5435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6c0db91957cd37d41741",
+    "url": "/static/js/main.41fd6f4d.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6c0db91957cd37d41741",
+    "url": "/static/css/main.aac06375.chunk.css"
+  },
+  {
+    "revision": "f3fe3eaa323b3c340929e6e36bf623bb",
+    "url": "/index.html"
+  }
+];
